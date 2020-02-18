0356d196e402b1c1962ec041a9f04cb4471cc766
Ben <bbjberg91@gmail.com>
2020-02-18 18:42:57 -0500

    not much done

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -1,15 +1,20 @@
 // You will add code to this file
-import React, { useState } from "react";
-import CommentInput from "./CommentInput";
-import Comment from "./Comment";
-import "./Comment.css";
+ import React, {useState} from "react";
+ import CommentInput from "./CommentInput";
+ import Comment from "./Comment";
+ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
-
+  console.log('console2',props)
+const [comments] = useState(props);
   return (
     <div>
+      {comments.man(p =>{
+        <Comment props={p}/>
+      })}
       {/* map through the comments data and return the Comment component */}
+      
       <CommentInput />
     </div>
   );
